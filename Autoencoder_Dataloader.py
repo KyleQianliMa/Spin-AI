afc89261fa6d85cbec5648894a8c2bcfdb423d09
Kyle Qianli Ma <41385391+KyleQianliMa@users.noreply.github.com>
2023-11-04 14:27:21 -0400

    Add files via upload

--- Autoencoder_Dataloader.py
+++ Autoencoder_Dataloader.py
@@ -19,29 +19,26 @@ from torch.utils.data import Dataset, DataLoader, ConcatDataset
 from torchvision import transforms, utils
 import scipy.io as spio
 import cv2
-
-
 import data_loader as ld
-#-------------------------Using data loader------------------------------
+import random
+
+#-------------------------Loading Experimental data as res---------
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-dataloader=ld.dataloader
-
-exp=np.loadtxt("exp data/yb2o3.txt")
-exp=exp[0:70,:]
-# for i in range(0,70):
-#     for j in range(0,470):
-#         if exp[i,j]<0.005 or exp[i,j]>0.05:
-#             exp[i,j]=0
+def Loadexp(exp,scale):
+    exp=exp[0:70,:]
+    # for i in range(0,70):
+    #     for j in range(0,470):
+    #         if exp[i,j]<0.005 or exp[i,j]>0.05:
+    #             exp[i,j]=0
 
 
-res = cv2.resize(exp, dsize=(250,249), interpolation=cv2.INTER_CUBIC)
-res=np.reshape(res,62250)
-
-scale=1000
-# plt.imshow(exp,cmap="jet",vmin=0,vmax=0.02,origin='lower')
-# plt.colorbar()
-# plt.show()
-res=torch.from_numpy(res*scale).to(torch.float32).to(device)
+    res = cv2.resize(exp, dsize=(250,249), interpolation=cv2.INTER_CUBIC)
+    res=np.reshape(res,62250)
+    # plt.imshow(exp,cmap="jet",vmin=0,vmax=0.02,origin='lower')
+    # plt.colorbar()
+    # plt.show()
+    res=torch.from_numpy(res*scale).to(torch.float32).to(device)
+    return res
 #-------------Autoencoder-----------------------------------------
 class AE(nn.Module):
     def __init__(self, **kwargs):
@@ -123,195 +120,39 @@ class AE(nn.Module):
 
         return reconstructed, f
 
+def Training(epochs,model_name,dataloader,PATH,res):
 
+    '''This function check if wether a model already exists in the PATH. If PATH exists it load and re-train and save model.
+       If not it train a new model and save it.
+    '''
 
-#%%----------------------Initial training the AE-----------------------
-if  __name__=='__main__':
-    epochs=5000
-
-    # create a model from `AE` autoencoder class
-    # load it to the specified device, either gpu or cpu
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = AE(input_shape=62250).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
     model.train()
-    # create an optimizer object
-    # Adam optimizer with learning rate 1e-3
-    optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
-
-
-    for epoch in range(epochs):
-        loss=0
-        # for i,data in enumerate(dataloader):
-        # i=0
-        for data_train, J_train in dataloader:
-            # data_train=torch.from_numpy(data_train).to(torch.float32).to(device,non_blocking=True)
-            # J_train=torch.from_numpy(J_train).to(torch.float32).to(device,non_blocking=True)
-            # data_train=data_train.to(torch.float32).to(device,non_blocking=True)
-            # J_train=J_train.to(torch.float32).to(device,non_blocking=True)
-            optimizer.zero_grad()
-            encoded_out, recon_out, J_out, reconstructed_exp = model.forward(data_train,res)
-
-            train_loss = model.custom_loss(J_train, recon_out, J_out, data_train, reconstructed_exp,res)
-
-
-            # compute accumulated gradients
-            train_loss.backward()
-
-            # perform parameter update based on current gradients
-            optimizer.step()
-
-            # add the mini-batch training loss to epoch loss
-            loss += train_loss.item()
-            # i=i+1
-            # print(i)
-            # display the epoch training loss
-        print("epoch : {}/{}, loss = {:.6f}".format(epoch + 1, epochs, loss))
-
-
-# model.eval()
-#%%--------------load experimental data--------------
-import matplotlib.pyplot as plt
-import numpy as np
-import scipy.io as spio
-from sklearn.decomposition import PCA
-from sklearn.model_selection import train_test_split
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import os
-import pandas as pd
-from torch.utils.data import Dataset, DataLoader, ConcatDataset
-from torchvision import transforms, utils
-import scipy.io as spio
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
-
-model.eval()
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-exp=np.loadtxt("exp data/yb2o3.txt")
-exp=exp[0:70,:]
-# for i in range(0,70):
-#     for j in range(0,470):
-#         if exp[i,j]<0.005 or exp[i,j]>0.05:
-#             exp[i,j]=0
-
-plt.imshow(exp,cmap="jet",vmin=0,vmax=0.02,origin='lower')
-res = cv2.resize(exp, dsize=(250,249), interpolation=cv2.INTER_CUBIC)
-
-scale=1000
-res=res*scale
-
-# res=expcaback
-res_reshape=np.reshape(res,62250)
-
-plt.imshow(res,cmap="jet",origin='lower')
-plt.colorbar()
-plt.show()
-
-res_reshape=torch.from_numpy(res_reshape).to(torch.float32).to(device)
-predicted,J_pred=model.inference(res_reshape)
-predicted=predicted.detach().cpu().numpy()
-predicted=np.reshape(predicted,(249,250))
-plt.imshow(predicted,cmap='jet', origin="lower",vmin=0,vmax=0.02*scale)
-plt.colorbar()
-plt.show()
-#%%---------testing AE----------
-import random
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# mat=spio.loadmat(r"Training\reduced\Mat_ebin\matsmall1.mat")
-# J=spio.loadmat(r"Training\reduced\J_ebin\Jsmall1.mat")
-
-mat=spio.loadmat(r"Training\matscale14.mat")
-J=spio.loadmat(r"Training\Jscale14.mat")
-
-mat=mat['Matrix']
-J=J['J']
-matT=mat.transpose((2,0,1))
-mat_reshape=np.reshape(matT,(1000,62250))
-
-data=mat_reshape
-J_train, J_test, data_train, data_test=train_test_split(J,data,test_size=0.2)
-J_train=torch.from_numpy(J_train).to(torch.float32).to(device)
-J_test=torch.from_numpy(J_test).to(torch.float32).to(device)
-data_train=torch.from_numpy(data_train).to(torch.float32).to(device)
-data_test=torch.from_numpy(data_test).to(torch.float32).to(device)
-for j in range(0,5):
-    i=random.randint(0,800)
-    predicted,J_pred=model.inference(data_train[i,:])
-    predicted=predicted.detach().cpu().numpy()
-    predicted=np.reshape(predicted,(1,249,250))
-    # plt.imshow(predicted[0,:,:],cmap='jet',vmin=0,vmax=10,origin='lower')
-    # plt.title('Autoencoder')
-    # plt.colorbar()
-    # plt.show()
-
-    test=np.reshape(data_train[i].cpu(),(1,249,250))
-    # plt.imshow(test[0,:,:],cmap='jet',vmin=0,vmax=10,origin='lower')
-    # plt.title('original')
-    # plt.colorbar()
-    # plt.show()
-
-    fig,(ax0,ax1)=plt.subplots(1,2,figsize=(10,10))
-
-    cb0=ax0.imshow(test[0,:,:],cmap='jet',vmin=0,vmax=20,origin='lower')
-    # ax0.set_title(J_train[i,:].detach().cpu().numpy().round(3),fontsize=8)
-    ax0.set_title(i,fontsize=8)
-    fig.colorbar(cb0,ax=ax0,shrink=0.4)
-
-    cb1=ax1.imshow(predicted[0,:,:],cmap='jet',vmin=0,vmax=20,origin='lower')
-    # ax1.set_title(J_pred.detach().cpu().numpy().round(3),fontsize=8)
-    fig.colorbar(cb1,ax=ax1,shrink=0.4)
-    print("---------------------------")
-    print('i=',i)
-    print("Original=",J_train[i,:].numpy(force=True).round(3))
-    print("Predicted=",J_pred.numpy(force=True).round(3))
-# %%-------------Save Model-----------------------------
-EPOCH = epochs
-# EPOCH = new_epochs
-PATH = r"R:\SpinwAI\model_J_scale.pt"
-LOSS = loss
-torch.save({
-            'epoch': EPOCH,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'loss': LOSS,
-            }, PATH)
-#%%-------------Load Model---------------------------------
-PATH = r"R:\SpinwAI\model_J_scale.pt"
-model = AE(input_shape=62250).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
-checkpoint = torch.load(PATH)
-model.load_state_dict(checkpoint['model_state_dict'])
-optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-epoch = checkpoint['epoch']
-loss = checkpoint['loss']
-model.train()
-#%%----------------resume training-----------------
-if  __name__=='__main__':
-    pre_epochs=epoch
-    new_epochs=pre_epochs+5000
-    # create a model from `AE` autoencoder class
-    # load it to the specified device, either gpu or cpu
-    # model = AE(input_shape=249500).to(device)
-    model.train()
-    # create an optimizer object
-    # Adam optimizer with learning rate 1e-3
-
+    #if model already exists, then we load previous model, re-train and save the model otherwise set pre-epochs=0 and start training
+    if os.path.exists(PATH):
+        checkpoint = torch.load(PATH)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        epoch = checkpoint['epoch']
+        loss = checkpoint['loss']
+        pre_epochs=epoch
+        new_epochs=pre_epochs+epochs
+        model.train()
+    else:
+        pre_epochs=0
+        new_epochs=epochs
 
     for epoch in range(pre_epochs,new_epochs):
         loss=0
-        # for i,data in enumerate(dataloader):
-        # i=0
+
         for data_train, J_train in dataloader:
-            # data_train=torch.from_numpy(data_train).to(torch.float32).to(device,non_blocking=True)
-            # J_train=torch.from_numpy(J_train).to(torch.float32).to(device,non_blocking=True)
-            # data_train=data_train.to(torch.float32).to(device,non_blocking=True)
-            # J_train=J_train.to(torch.float32).to(device,non_blocking=True)
+
             optimizer.zero_grad()
             encoded_out, recon_out, J_out, reconstructed_exp = model.forward(data_train,res)
 
             train_loss = model.custom_loss(J_train, recon_out, J_out, data_train, reconstructed_exp,res)
-
 
             # compute accumulated gradients
             train_loss.backward()
@@ -322,16 +163,123 @@ if  __name__=='__main__':
             # add the mini-batch training loss to epoch loss
             loss += train_loss.item()
         print("epoch : {}/{}, loss = {:.6f}".format(epoch + 1, new_epochs, loss))
+    EPOCH = new_epochs
+    LOSS = loss
+    torch.save({
+                'epoch': EPOCH,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': LOSS,
+                }, PATH)
+
+    return model, loss, optimizer
 
 
-#%%-------------Save Model-----------------------------
-# EPOCH = epochs
-EPOCH = new_epochs
-PATH = r"R:\SpinwAI\model_J_scale.pt"
-LOSS = loss
-torch.save({
-            'epoch': EPOCH,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'loss': LOSS,
-            }, PATH)
+
+def Testing(model_name,mat,J,count, PATH):
+    '''This function load a model and print testing images
+
+       model_name: pre-saved model name. model_name only  represents the material name
+       mat_test: The path and name for testing data
+       J_test:   The path and name for testing labels
+       counts:   Number of random selected images user want to generate
+
+       '''
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = AE(input_shape=62250).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    checkpoint = torch.load(PATH)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch = checkpoint['epoch']
+    loss = checkpoint['loss']
+    model.eval()
+
+    mat=mat['Matrix']
+    J=J['J']
+    matT=mat.transpose((2,0,1))
+    mat_reshape=np.reshape(matT,(1000,62250))
+
+    data=mat_reshape
+    J_train, J_test, data_train, data_test=train_test_split(J,data,test_size=0.2)
+    J_train=torch.from_numpy(J_train).to(torch.float32).to(device)
+    J_test=torch.from_numpy(J_test).to(torch.float32).to(device)
+    data_train=torch.from_numpy(data_train).to(torch.float32).to(device)
+    data_test=torch.from_numpy(data_test).to(torch.float32).to(device)
+    for j in range(0,5):
+        i=random.randint(0,800)
+        predicted,J_pred=model.inference(data_train[i,:])
+        predicted=predicted.detach().cpu().numpy()
+        predicted=np.reshape(predicted,(1,249,250))
+        # plt.imshow(predicted[0,:,:],cmap='jet',vmin=0,vmax=10,origin='lower')
+        # plt.title('Autoencoder')
+        # plt.colorbar()
+        # plt.show()
+
+        test=np.reshape(data_train[i].cpu(),(1,249,250))
+        # plt.imshow(test[0,:,:],cmap='jet',vmin=0,vmax=10,origin='lower')
+        # plt.title('original')
+        # plt.colorbar()
+        # plt.show()
+
+        fig,(ax0,ax1)=plt.subplots(1,2,figsize=(10,10))
+
+        cb0=ax0.imshow(test[0,:,:],cmap='jet',vmin=0,vmax=20,origin='lower')
+        # ax0.set_title(J_train[i,:].detach().cpu().numpy().round(3),fontsize=8)
+        ax0.set_title(i,fontsize=8)
+        fig.colorbar(cb0,ax=ax0,shrink=0.4)
+
+        cb1=ax1.imshow(predicted[0,:,:],cmap='jet',vmin=0,vmax=20,origin='lower')
+        # ax1.set_title(J_pred.detach().cpu().numpy().round(3),fontsize=8)
+        fig.colorbar(cb1,ax=ax1,shrink=0.4)
+        print("---------------------------")
+        print('i=',i)
+        print("Original=",J_train[i,:].numpy(force=True).round(3))
+        print("Predicted=",J_pred.numpy(force=True).round(3))
+
+def test_exp(exp,model):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    exp=exp[0:70,:]
+    # for i in range(0,70):
+    #     for j in range(0,470):
+    #         if exp[i,j]<0.005 or exp[i,j]>0.05:
+    #             exp[i,j]=0
+
+    plt.imshow(exp,cmap="jet",vmin=0,vmax=0.02,origin='lower')
+    res = cv2.resize(exp, dsize=(250,249), interpolation=cv2.INTER_CUBIC)
+
+    scale=1000
+    res=res*scale
+
+    # res=expcaback
+    res_reshape=np.reshape(res,62250)
+
+    plt.imshow(res,cmap="jet",origin='lower')
+    plt.colorbar()
+    plt.show()
+
+    res_reshape=torch.from_numpy(res_reshape).to(torch.float32).to(device)
+    predicted,J_pred=model.inference(res_reshape)
+    predicted=predicted.detach().cpu().numpy()
+    predicted=np.reshape(predicted,(249,250))
+    plt.imshow(predicted,cmap='jet', origin="lower",vmin=0,vmax=0.02*scale)
+    plt.colorbar()
+    plt.show()
+#%%--------------load experimental data--------------
+if  __name__=='__main__':
+    exp=np.loadtxt("exp data/yb2o3.txt")
+    dataloader=ld.dataloader
+    model_name='Yb2O3.pt' #only contains the material name and .pt
+    PATH = r"R:\SpinwAI\Yb2O3\model_"+model_name
+    epochs=5 #change this number to around 1000 for initial training
+    model,loss,optimizer=Training(epochs,model_name,dataloader,PATH)
+
+    model_name='Yb2O3.pt' #change model name and path if we want to test different models
+    PATH = r"R:\SpinwAI\Yb2O3\model_"+model_name
+    mat_test=spio.loadmat(r"R:\SpinwAI\Yb2O3\Matscale\matscale01.mat") #01-19 sets of data have been seen. 20th data set is not seen
+    J_test=spio.loadmat(r"R:\SpinwAI\Yb2O3\Jscale\Jscale01.mat") #01-19 sets of data have been seen. 20th data set is not seen
+    count=2
+    Testing(model_name,mat_test,J_test,count)
+
+    #testing on expermental data
+    test_exp(exp,model)
--- data_loader.py
+++ data_loader.py
@@ -14,6 +14,32 @@ from torchvision import transforms, utils
 import scipy.io as spio
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #-------------------------Using data loader------------------------------
+
+# class NeutronData(Dataset):
+#     def __init__(self, matlab_matrix, J_matrix, root_dir, transform=None):
+#         self.mat=np.loadtxt(matlab_matrix)
+#         self.J=np.loadtxt(J_matrix)
+#         self.root_dir=root_dir
+#         self.transform=transform
+
+#     def __len__(self):
+#         return len(self.J)
+
+#     def __getitem__(self, idx):
+#         if torch.is_tensor(idx):
+#             idx = idx.tolist()
+
+#         image=self.mat[idx]
+#         J_par=self.J[idx]
+
+#         if self.transform:
+#             # image=np.array(image)
+#             # image= np.reshape(image,249500)
+#             # J_par=np.array(J_par)
+#             image=torch.from_numpy(image).to(torch.float32).to(device)
+#             J_par=torch.from_numpy(J_par).to(torch.float32).to(device)
+#         return image, J_par
+
 class NeutronData(Dataset):
     def __init__(self, matlab_matrix,Mkey, J_matrix, Jkey, root_dir, transform=None):
 
@@ -41,7 +67,82 @@ class NeutronData(Dataset):
             image=torch.from_numpy(image).to(torch.float32).to(device)
             J_par=torch.from_numpy(J_par).to(torch.float32).to(device)
         return image, J_par
-        
+
+# all_data=[]
+
+# neutron_dataset1 = NeutronData(matlab_matrix=r"Training/reduced/matsmall1.mat",
+#                                   Mkey='Matrix',
+#                                 J_matrix=r"Training/reduced/Jsmall1.mat",
+#                                 Jkey='J',
+#                                 root_dir="SpinwAI/",transform=True)
+# all_data.append(neutron_dataset1)
+# del neutron_dataset1
+
+# neutron_dataset2 = NeutronData(matlab_matrix=r"Training/reduced/matsmall2.mat",
+#                                   Mkey='Matrix',
+#                                 J_matrix=r"Training/reduced/Jsmall2.mat",
+#                                 Jkey='J',
+#                                 root_dir="SpinwAI/",transform=True)
+# all_data.append(neutron_dataset2)
+# del neutron_dataset2
+
+
+# neutron_dataset3 = NeutronData(matlab_matrix=r"Training/reduced/matsmall3.mat",
+#                                  Mkey='Matrix',
+#                                 J_matrix=r"Training/reduced/Jsmall3.mat",
+#                                 Jkey='J',
+#                                 root_dir="SpinwAI/",transform=True)
+# all_data.append(neutron_dataset3)
+# del neutron_dataset3
+
+
+# neutron_dataset4 = NeutronData(matlab_matrix=r"Training/reduced/matsmall4.mat",
+#                                  Mkey='Matrix',
+#                                 J_matrix=r"Training/reduced/Jsmall4.mat",
+#                                 Jkey='J',
+#                                 root_dir="SpinwAI/",transform=True)
+# all_data.append(neutron_dataset4)
+# del neutron_dataset4
+
+# neutron_dataset5 = NeutronData(matlab_matrix=r"Training/reduced/matsmall5.mat",
+#                                  Mkey='Matrix',
+#                                 J_matrix=r"Training/reduced/Jsmall5.mat",
+#                                 Jkey='J',
+#                                 root_dir="SpinwAI/",transform=True)
+# all_data.append(neutron_dataset5)
+# del neutron_dataset5
+
+# neutron_dataset6 = NeutronData(matlab_matrix=r"Training/reduced/matsmall6.mat",
+#                                  Mkey='Matrix',
+#                                 J_matrix=r"Training/reduced/Jsmall6.mat",
+#                                 Jkey='J',
+#                                 root_dir="SpinwAI/",transform=True)
+# all_data.append(neutron_dataset6)
+# del neutron_dataset6
+
+# neutron_dataset7 = NeutronData(matlab_matrix=r"Training/reduced/matsmall7.mat",
+#                                  Mkey='Matrix',
+#                                 J_matrix=r"Training/reduced/Jsmall7.mat",
+#                                 Jkey='J',
+#                                 root_dir="SpinwAI/",transform=True)
+# all_data.append(neutron_dataset7)
+# del neutron_dataset7
+
+# neutron_dataset8 = NeutronData(matlab_matrix=r"Training/reduced/matsmall8.mat",
+#                                  Mkey='Matrix',
+#                                 J_matrix=r"Training/reduced/Jsmall8.mat",
+#                                 Jkey='J',
+#                                 root_dir="SpinwAI/",transform=True)
+# all_data.append(neutron_dataset8)
+# del neutron_dataset8
+
+# neutron_dataset9 = NeutronData(matlab_matrix=r"Training/reduced/matsmall9.mat",
+#                                  Mkey='Matrix',
+#                                 J_matrix=r"Training/reduced/Jsmall9.mat",
+#                                 Jkey='J',
+#                                 root_dir="SpinwAI/",transform=True)
+# all_data.append(neutron_dataset9)
+# del neutron_dataset9
 Jdir = os.listdir(r'R:\SpinwAI\Training\reduced\Jscale')
 Matdir = os.listdir(r'R:\SpinwAI\Training\reduced\Matscale')
 all_data=[]
